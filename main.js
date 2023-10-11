f4a6d803bafc67aece41a32b29f9ce5ddc500eb9
Ahmedemm <falkonne97@gmail.com>
2023-10-12 01:09:17 +0200

    FF

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 function toggleMenu() {
-  const navLinks = document.getElementById('nav-links');
-  navLinks.style.display = (navLinks.style.display === 'block') ? 'none' : 'block';
+  const navLinks = document.getElementById("nav-links");
+  navLinks.style.display =
+    navLinks.style.display === "block" ? "none" : "block";
 }
+
+function openImageModal() {
+  var modal = document.getElementById("imageModal");
+  var modalImage = document.getElementById("modalImage");
+  modal.style.display = "block";
+  modalImage.src = this.src;
+}
+
+function closeImageModal() {
+  var modal = document.getElementById("imageModal");
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  var modal = document.getElementById("imageModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
